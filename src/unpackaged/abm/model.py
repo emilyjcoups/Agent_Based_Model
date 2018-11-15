ddadf519e11f877b8893bbaa18fc632abd27491e
Emily Coupland <ejcoupland1@sheffield.ac.uk>
2018-11-15 15:56:49 +0000

    Coordinates now based on 100x100 grid
    y and x coordinates previously based on starting point of 50, now they are based on random integers between 0 and 99

--- src/unpackaged/abm/model.py
+++ src/unpackaged/abm/model.py
@@ -11,45 +11,45 @@ import random
 
 ## Declare y and x variables 
     
-y0, x0 = 50, 50 
+y0, x0 = random.randint(0, 99), random.randint(0, 99)
 
 ## Change y and x based on random numbers (1/3)
 
 if random.random() < 0.5:
-    y0 =+ 1
+    y0 += 1
 else:
-    y0 =- 1
+    y0 -= 1
     
 if random.random() < 0.5:
-    x0 =+ 1
+    x0 += 1
 else:
-    x0 =- 1
+    x0 -= 1
 
 
 ## Repeat change to y and x based on random numbers (2/3)
 
 if random.random() < 0.5:
-    y0 =+ 1
+    y0 += 1
 else:
-    y0 =- 1
+    y0 -= 1
     
 if random.random() < 0.5:
-    x0 =+ 1
+    x0 += 1
 else:
-    x0 =- 1
+    x0 -= 1
 
 
 ## Repeat change to y and x based on random numbers (3/3)
 
 if random.random() < 0.5:
-    y0 =+ 1
+    y0 += 1
 else:
-    y0 =- 1
+    y0 -= 1
     
 if random.random() < 0.5:
-    x0 =+ 1
+    x0 += 1
 else:
-    x0 =- 1
+    x0 -= 1
 
 # Check y0 and x0 values 
     
@@ -61,45 +61,45 @@ print(y0, x0)
 
 ## Declare y and x variables 
     
-y1, x1 = 50, 50 
+y1, x1 = random.randint(0, 99), random.randint(0, 99)
 
 ## Change y and x based on random numbers (1/3)
 
 if random.random() < 0.5:
-    y1 =+ 1
+    y1 += 1
 else:
-    y1 =- 1
+    y1 -= 1
     
 if random.random() < 0.5:
-    x1 =+ 1
+    x1 += 1
 else:
-    x1 =- 1
+    x1 -= 1
 
 
 ## Repeat change to y and x based on random numbers (2/3)
 
 if random.random() < 0.5:
-    y1 =+ 1
+    y1 += 1
 else:
-    y1 =- 1
+    y1 -= 1
     
 if random.random() < 0.5:
-    x1 =+ 1
+    x1 += 1
 else:
-    x1 =- 1
+    x1 -= 1
 
 
 ## Repeat change to y and x based on random numbers (3/3)
 
 if random.random() < 0.5:
-    y1 =+ 1
+    y1 += 1
 else:
-    y1 =- 1
+    y1 -= 1
     
 if random.random() < 0.5:
-    x1 =+ 1
+    x1 += 1
 else:
-    x1 =- 1
+    x1 -= 1
 
 # Check y1 and x1 values
 
@@ -110,7 +110,6 @@ print(y1, x1)
 answer = (((y0 - y1)**2) + ((x0 - x1)**2))**0.5 
 
 print(answer)
-
 
 # Make a second set of y and xs, and make these change randomly as well.
 # Work out the distance between the two sets of y and xs.
