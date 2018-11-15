4dc5a024c6efe17b9c9924b5fdad3bfc1f0bf786
Emily Coupland <ejcoupland1@sheffield.ac.uk>
2018-11-15 17:04:28 +0000

    Added for loops to simply creation of agents and to move each agent a specified number of times

--- src/unpackaged/abm/model.py
+++ src/unpackaged/abm/model.py
@@ -11,127 +11,50 @@ import random
 import operator
 import matplotlib.pyplot 
 
-# List of agents
+# Variable holding maximum number of agents 
 
+max_agents = 10
+max_iterations = 100
+
+# List of agents
 agents =[]
 
-# AGENT ONE
+# Creates agents (as many as max_agents value) with random coordinates (based on 100 x 100 grid) and adds them to list of agents
+for i in range(max_agents):
+    agents.append([random.randint(0, 99), random.randint(0, 99)])
+    
+# Moves each agent randomly 
+for i in range(max_iterations):
+    for i in range(max_agents):
+        if random.random() < 0.5:
+            agents[i][0] += 1
+        else:
+            agents[i][0] -= 1
+            
+        if random.random() < 0.5:
+            agents[i][1] += 1
+        else:
+            agents[i][1] -= 1
 
-## Add random y and x coordinates (based on 100x100 grid) to list of agents
-
-agents.append([random.randint(0, 99), random.randint(0, 99)])
-
-## Change y and x based on random numbers (1/3)
 
 print(agents)
 
-if random.random() < 0.5:
-    agents[0][0] += 1
-else:
-    agents[0][0] -= 1
-    
-if random.random() < 0.5:
-    agents[0][1] += 1
-else:
-    agents[0][1] -= 1
-
-
-## Repeat change to y and x based on random numbers (2/3)
-
-if random.random() < 0.5:
-    agents[0][0] += 1
-else:
-    agents[0][0] -= 1
-    
-if random.random() < 0.5:
-    agents[0][1] += 1
-else:
-    agents[0][1] -= 1
-
-
-## Repeat change to y and x based on random numbers (3/3)
-
-if random.random() < 0.5:
-    agents[0][0] += 1
-else:
-    agents[0][0] -= 1
-    
-if random.random() < 0.5:
-    agents[0][1] += 1
-else:
-    agents[0][1] -= 1
-
-# Check y0 and x0 values 
-    
-print(agents)
-
-
-
-# AGENT TWO
-
-## Add random y and x coordinates (based on 100x100 grid) to list of agents
-
-agents.append([random.randint(0, 99), random.randint(0, 99)])
-
-## Change y and x based on random numbers (1/3)
-
-print(agents)
-
-if random.random() < 0.5:
-    agents[1][0] += 1
-else:
-    agents[1][0] -= 1
-    
-if random.random() < 0.5:
-    agents[1][1] += 1
-else:
-    agents[1][1] -= 1
-
-
-## Repeat change to y and x based on random numbers (2/3)
-
-if random.random() < 0.5:
-    agents[1][0] += 1
-else:
-    agents[1][0] -= 1
-    
-if random.random() < 0.5:
-    agents[1][1] += 1
-else:
-    agents[1][1] -= 1
-
-
-## Repeat change to y and x based on random numbers (3/3)
-
-if random.random() < 0.5:
-    agents[1][0] += 1
-else:
-    agents[1][0] -= 1
-    
-if random.random() < 0.5:
-    agents[1][1] += 1
-else:
-    agents[1][1] -= 1
-
-# Check y1 and x1 values
-
-print(agents)
-
-# Calculate Euclidian distance between agents 
-
+''' Calculate Euclidian distance between agents 
 answer = (((agents[0][0] - agents[1][0])**2) + ((agents[0][1] - agents[1][1])**2))**0.5 
 
 print(answer)
-
-eastern_agent = (max(agents, key= operator.itemgetter(1)))
+'''
 
 # Plots agent locations
 
 matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.xlim(0, 99)
-matplotlib.pyplot.scatter(agents[0][1],agents[0][0])
-matplotlib.pyplot.scatter(agents[1][1],agents[1][0])
+for i in range(max_agents):
+    matplotlib.pyplot.scatter(agents[i][1],agents[i][0])
+'''
+eastern_agent = (max(agents, key= operator.itemgetter(1)))
 matplotlib.pyplot.scatter(eastern_agent[1], eastern_agent[0], color='red')
+'''
 matplotlib.pyplot.show()
 
 # Make a second set of y and xs, and make these change randomly as well.
