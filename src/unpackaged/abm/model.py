a0b222fad34a1e562aba5f622d00d7345d5357e4
Emily Coupland <ejcoupland1@sheffield.ac.uk>
2018-11-15 15:17:00 +0000

    Second agent created, programmed to move randomly 3 times

--- src/unpackaged/abm/model.py
+++ src/unpackaged/abm/model.py
@@ -7,12 +7,11 @@ This is a temporary script file.
 
 import random
 
+### AGENT ONE
+
 # Declare y and x variables 
     
-y0, x0 = 50, 50
-
-# Generate random number 
-
+y0, x0 = 50, 50 
 
 # Change y and x based on random numbers (1/3)
 
@@ -55,6 +54,57 @@ else:
     x0 =- 1
 
 print(y0, x0)
+
+
+
+### AGENT TWO
+
+# Declare y and x variables 
+    
+y1, x1 = 50, 50 
+
+# Change y and x based on random numbers (1/3)
+
+if random.random() < 0.5:
+    y1 =+ 1
+else:
+    y1 =- 1
+    
+if random.random() < 0.5:
+    x1 =+ 1
+else:
+    x1 =- 1
+
+print(y1, x1)
+
+# Repeat change to y and x based on random numbers (2/3)
+
+if random.random() < 0.5:
+    y1 =+ 1
+else:
+    y1 =- 1
+    
+if random.random() < 0.5:
+    x1 =+ 1
+else:
+    x1 =- 1
+
+print(y1, x1)
+
+# Repeat change to y and x based on random numbers (3/3)
+
+if random.random() < 0.5:
+    y1 =+ 1
+else:
+    y1 =- 1
+    
+if random.random() < 0.5:
+    x1 =+ 1
+else:
+    x1 =- 1
+
+print(y1, x1)
+
 
 # Make a second set of y and xs, and make these change randomly as well.
 # Work out the distance between the two sets of y and xs.
