2ba65c84884945fc6deb31c5c5b162f95054d084
Emily Coupland <ejcoupland1@sheffield.ac.uk>
2018-11-15 16:24:29 +0000

    Adds agents list. Agent coordinates now accessed through indexing

--- src/unpackaged/abm/model.py
+++ src/unpackaged/abm/model.py
@@ -7,107 +7,115 @@ This is a temporary script file.
 
 import random
 
+# List of agents
+
+agents =[]
+
 # AGENT ONE
 
-## Declare y and x variables 
-    
-y0, x0 = random.randint(0, 99), random.randint(0, 99)
+## Add random y and x coordinates (based on 100x100 grid) to list of agents
+
+agents.append([random.randint(0, 99), random.randint(0, 99)])
 
 ## Change y and x based on random numbers (1/3)
 
+print(agents)
+
 if random.random() < 0.5:
-    y0 += 1
+    agents[0][0] += 1
 else:
-    y0 -= 1
+    agents[0][0] -= 1
     
 if random.random() < 0.5:
-    x0 += 1
+    agents[0][1] += 1
 else:
-    x0 -= 1
+    agents[0][1] -= 1
 
 
 ## Repeat change to y and x based on random numbers (2/3)
 
 if random.random() < 0.5:
-    y0 += 1
+    agents[0][0] += 1
 else:
-    y0 -= 1
+    agents[0][0] -= 1
     
 if random.random() < 0.5:
-    x0 += 1
+    agents[0][1] += 1
 else:
-    x0 -= 1
+    agents[0][1] -= 1
 
 
 ## Repeat change to y and x based on random numbers (3/3)
 
 if random.random() < 0.5:
-    y0 += 1
+    agents[0][0] += 1
 else:
-    y0 -= 1
+    agents[0][0] -= 1
     
 if random.random() < 0.5:
-    x0 += 1
+    agents[0][1] += 1
 else:
-    x0 -= 1
+    agents[0][1] -= 1
 
 # Check y0 and x0 values 
     
-print(y0, x0)
+print(agents)
 
 
 
 # AGENT TWO
 
-## Declare y and x variables 
-    
-y1, x1 = random.randint(0, 99), random.randint(0, 99)
+## Add random y and x coordinates (based on 100x100 grid) to list of agents
+
+agents.append([random.randint(0, 99), random.randint(0, 99)])
 
 ## Change y and x based on random numbers (1/3)
 
+print(agents)
+
 if random.random() < 0.5:
-    y1 += 1
+    agents[1][0] += 1
 else:
-    y1 -= 1
+    agents[1][0] -= 1
     
 if random.random() < 0.5:
-    x1 += 1
+    agents[1][1] += 1
 else:
-    x1 -= 1
+    agents[1][1] -= 1
 
 
 ## Repeat change to y and x based on random numbers (2/3)
 
 if random.random() < 0.5:
-    y1 += 1
+    agents[1][0] += 1
 else:
-    y1 -= 1
+    agents[1][0] -= 1
     
 if random.random() < 0.5:
-    x1 += 1
+    agents[1][1] += 1
 else:
-    x1 -= 1
+    agents[1][1] -= 1
 
 
 ## Repeat change to y and x based on random numbers (3/3)
 
 if random.random() < 0.5:
-    y1 += 1
+    agents[1][0] += 1
 else:
-    y1 -= 1
+    agents[1][0] -= 1
     
 if random.random() < 0.5:
-    x1 += 1
+    agents[1][1] += 1
 else:
-    x1 -= 1
+    agents[1][1] -= 1
 
 # Check y1 and x1 values
 
-print(y1, x1)
+print(agents)
 
 # Calculate Euclidian distance between agents 
 
-answer = (((y0 - y1)**2) + ((x0 - x1)**2))**0.5 
+answer = (((agents[0][0] - agents[1][0])**2) + ((agents[0][1] - agents[1][1])**2))**0.5 
 
 print(answer)
 
