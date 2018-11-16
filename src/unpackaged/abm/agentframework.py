b6b3aa36299149d515ba6d3438d15e569d356e0f
Emily Coupland <ejcoupland1@sheffield.ac.uk>
2018-11-16 20:45:53 +0000

    Agent class created

---
+++ src/unpackaged/abm/agentframework.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 16 13:26:36 2018
+
+@author: emilycoupland
+"""
+
+import random
+
+class Agent:
+    def __init__ (self):
+        self.x = random.randint(0,99)
+        self.y = random.randint(0,99)
+        
+    def move(self):
+        if random.random() < 0.5:
+            self.y = (self.y + 1) % 100
+        else:
+            self.y = (self.y - 1) % 100
+        
+        if random.random() < 0.5:
+            self.x = (self.x + 1) % 100
+        else:
+            self.x = (self.x - 1) % 100
+            
+            
--- src/unpackaged/abm/model.py
+++ src/unpackaged/abm/model.py
@@ -7,16 +7,16 @@ This is a temporary script file.
 
 # Import libraries
 
-import random
 import operator
 import matplotlib.pyplot 
 import time
+import agentframework
 
 # Calculates distance between agents 
 
 def distance_between(agents_row_a, agents_row_b):
-    print(agents_row_a, agents_row_b) 
-    return (((agents_row_a[0] - agents_row_b[0])**2) + ((agents_row_a[1] - agents_row_b[1])**2))**0.5 
+    # print(agents_row_a.x, agents_row_b.x) 
+    return (((agents_row_a.x - agents_row_b.x)**2) + ((agents_row_a.y - agents_row_b.y)**2))**0.5 
 
 # Variable holding maximum number of agents 
 
@@ -28,50 +28,35 @@ agents =[]
 
 # Creates agents (as many as max_agents value) with random coordinates (based on 100 x 100 grid) and adds them to list of agents
 for i in range(max_agents):
-    agents.append([random.randint(0, 100), random.randint(0, 100)])
+    agents.append(agentframework.Agent())
+    # agents.append([random.randint(0, 100), random.randint(0, 100)])
     
 # Moves each agent randomly 
 for j in range(max_iterations):
     for i in range(max_agents):
-        if random.random() < 0.5:
-            agents[i][0] = (agents[i][0] + 1) % 100
-        else:
-            agents[i][0] = (agents[i][0] - 1) % 100
-            
-        if random.random() < 0.5:
-            agents[i][1] = (agents[i][1] + 1) % 100
-        else:
-            agents[i][1] = (agents[i][1] - 1) % 100
-
+        agents[i].move()
 
 # print(agents)
 
+''' Testing
+a = agentframework.Agent()
+print(a.y, a.x)
 
+a.move()
+print(a.y, a.x)
+''' 
 
 # Plots agent locations
 
-matplotlib.pyplot.ylim(0, 100)
-matplotlib.pyplot.xlim(0, 100)
+matplotlib.pyplot.ylim(0, 99)
+matplotlib.pyplot.xlim(0, 99)
 for i in range(max_agents):
-    matplotlib.pyplot.scatter(agents[i][1],agents[i][0])
-'''
-eastern_agent = (max(agents, key= operator.itemgetter(1)))
-matplotlib.pyplot.scatter(eastern_agent[1], eastern_agent[0], color='red')
-'''
+    matplotlib.pyplot.scatter(agents[i].x, agents[i].y)
 matplotlib.pyplot.show()
 
-
-# Calculate Euclidian distance between each pair of agents. 
-# NOTE TO SELF: TIMER NEEDS UPDATED, DEPRECATED in PYTHON 3.3
-            
-start = time.clock()
 
 # Note to self: use agent in agents?
 for k in range(max_agents): 
     for l in range(max_agents): 
         distance = distance_between(agents[l], agents[k])
-        print (distance)
-
-end = time.clock()
-
-print("time = " + str(end - start))
+        # print (distance)
